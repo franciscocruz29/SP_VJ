3d64dcec7df914cdda08faff36a8623b813a8e1f
franciscocruz29 <franciscohuertas25@gmail.com>
2023-04-04 09:29:56 -0500

    Added pseudocode for the button event

---
+++ index.js
@@ -0,0 +1,10 @@
+function addButtonEvent() {
+  document.getElementById("searchButton").addEventListener("click", function () {
+    // (4) remove old list if there already is a list
+    // (1) get value from the input field
+    // (2) searcg list from API with value
+    // (3) append list to DOM
+  });
+}
+
+addButtonEvent();
